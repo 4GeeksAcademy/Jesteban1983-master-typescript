6d01450d366a9a809be6b0acdfbdb2efffc8d018
Jesteban1983 <joonathanesteban@protonmail.com>
2026-05-03 13:27:07 +0200

    Avance

--- exercises/101-computeCompoundInterest/app.js
+++ exercises/101-computeCompoundInterest/app.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function computeCompoundInterest(principal, interestRate, compoundingFrequency, timeInYears) {
-    // your code here
-    return 0;
+    let total = principal * Math.pow((1 + interestRate / compoundingFrequency), (compoundingFrequency * timeInYears));
+    let interes = total - principal;
+    return interes;
 }
+let output = computeCompoundInterest(1500, 0.043, 4, 6);
+console.log(output);
--- exercises/101-computeCompoundInterest/app.ts
+++ exercises/101-computeCompoundInterest/app.ts
@@ -1,10 +1,4 @@
 function computeCompoundInterest(principal: number, interestRate: number, compoundingFrequency: number, timeInYears: number): number {
-  // your code here
-<<<<<<< HEAD
-  return 0;
-}
-
-=======
   let total = principal * Math.pow((1 + interestRate / compoundingFrequency), (compoundingFrequency * timeInYears))
   let interes = total - principal
 
@@ -12,5 +6,4 @@ function computeCompoundInterest(principal: number, interestRate: number, compou
 }
 let output = computeCompoundInterest(1500, 0.043, 4, 6);
 console.log(output);
->>>>>>> 24b38f03 (Avance)
 export {};
