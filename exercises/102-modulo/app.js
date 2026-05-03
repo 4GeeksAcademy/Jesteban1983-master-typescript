c2d76c7f7e67b7ec4f31eb2e3c2772a0352bcadc
Jesteban1983 <joonathanesteban@protonmail.com>
2026-05-03 13:28:31 +0200

    Avance 102

--- exercises/102-modulo/app.js
+++ exercises/102-modulo/app.js
@@ -4,3 +4,5 @@ function modulo(num1, num2) {
     // your code here
     return 0;
 }
+let output = modulo(25, 4);
+console.log(output);
--- exercises/102-modulo/app.ts
+++ exercises/102-modulo/app.ts
@@ -2,5 +2,6 @@ function modulo(num1: number, num2: number): number {
   // your code here
   return 0;
 }
-
+let output= modulo(25,4);
+console.log(output);
 export {};
