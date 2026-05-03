8f1f31e7f7138e5be32dd1fe13e53a57b1987039
Jesteban1983 <joonathanesteban@protonmail.com>
2026-05-03 13:04:11 +0200

    Avance

--- exercises/101-computeCompoundInterest/app.ts
+++ exercises/101-computeCompoundInterest/app.ts
@@ -1,6 +1,16 @@
 function computeCompoundInterest(principal: number, interestRate: number, compoundingFrequency: number, timeInYears: number): number {
   // your code here
+<<<<<<< HEAD
   return 0;
 }
 
+=======
+  let total = principal * Math.pow((1 + interestRate / compoundingFrequency), (compoundingFrequency * timeInYears))
+  let interes = total - principal
+
+  return interes;
+}
+let output = computeCompoundInterest(1500, 0.043, 4, 6);
+console.log(output);
+>>>>>>> 24b38f03 (Avance)
 export {};
