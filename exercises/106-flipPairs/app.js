3e634d3dbc8725b57268a6f2f17270cef0a5d720
Jesteban1983 <joonathanesteban@protonmail.com>
2026-05-03 16:03:29 +0200

    Ejercicio 107

--- exercises/106-flipPairs/app.js
+++ exercises/106-flipPairs/app.js
@@ -1,6 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function flipPairs(input) {
-    // your code here
-    return '';
+    let result = "";
+    for (let i = 0; i < input.length; i += 2) {
+        if (input[i + 1] !== undefined) {
+            result += input[i + 1] + input[i];
+        }
+        else {
+            result += input[i];
+        }
+    }
+    return result;
 }
+let input = 'Jonathan';
+let output = flipPairs(input);
+console.log(output);
--- exercises/106-flipPairs/app.ts
+++ exercises/106-flipPairs/app.ts
@@ -1,6 +1,16 @@
 function flipPairs(input: string): string {
-  // your code here
-  return '';
-}
+  let result: string = "";
+  for (let i = 0; i < input.length; i += 2){
 
+    if (input[i + 1] !== undefined){
+      result += input[i + 1] + input[i];
+    } else {
+      result += input[i];
+    }
+  }
+  return result;
+}
+let input = 'Jonathan';
+let output = flipPairs(input);
+console.log(output);
 export {};
